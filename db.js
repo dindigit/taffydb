2f8eac22858297b2bf938f872272c87b56e763ca
dindinet <github.dklongley@neverbox.com>
2016-10-03 21:07:26 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var product_db = TAFFY([
+var products = TAFFY([
   { "item"  : 1,
     "name"  : "Blue Ray Player",
     "price" : 99.99
@@ -6,5 +6,9 @@ var product_db = TAFFY([
   { "item"  : 2,
     "name"  : "3D TV",
     "price" : 1799.99
+  },
+  { "item"  : 3,
+    "name"  : "Computer",
+    "price" : 450.99
   }
 ]);
